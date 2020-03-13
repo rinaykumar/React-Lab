86dba32e5291fd20b3984a950d9ff61b2c3fc0d5
rinaykumar <rinaykumar@gmail.com>
2020-03-12 21:13:17 -0700

    react lab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const test = <div>Hello</div>;
 
 const App = () => {
+  // pass in default value to useState
+  const [note, setNote] = React.useState(''); // state variable + setter
+  const [notes, setNotes] = React.useState([]);
+
+  const fetchNotes = () => {
+    // get all notes
+    axios.get('/api/getAllNotes')
+      .then((res) => {
+        console.log(res);
+        setNotes(res.data.notes);
+      })
+      .catch(console.log);
+  };
+
+  const submitNote = () => { // arrow function
+    console.log(note);
+    const body = {
+      note: note
+    };
+    axios.post('/api/addNote', body)
+      .then(() => setNote(''))
+      .then(() => fetchNotes()) // fetch notes
+      .catch(console.log);
+  };
+
+  // hook
+  React.useEffect(() => {
+      fetchNotes();
+  }, []);
+
+  // jsx
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>
+            <input value={note} onChange={e => setNote(e.target.value)}/>
+          </div>
+          <div>
+            <button onClick={submitNote}>Add Note</button>
+          </div>
+          <div>
+            {notes.map((item) => {
+              // convert array
+              return (
+                <div>
+                  {item}
+                </div>
+              );
+            })} 
+          </div>
+        </div>
       </header>
     </div>
   );
